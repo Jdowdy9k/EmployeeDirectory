f1d8dae1e15a7d6384ca8cf19eaa81ef0fccd285
Jdowdy <Jdowdy9k@gamil.com>
2020-07-02 18:19:16 -0400

    worked components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, } from "react-router-dom";
 import Main from "./components/Main/index"
 import Header from "./components/Header/index"
 
